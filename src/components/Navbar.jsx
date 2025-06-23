d62a77624d9672df81b8485f71d8de3acd79f472
BriArl <brianna.youte@gmail.com>
2025-06-23 02:55:44 -0400

    fixed nav bar typo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-[#24252a] text-[18px] font-bold cursor-pointer flex '>
-            Brianna &nbsp;
+            Brianna's &nbsp;
             <span className='sm:block hidden'> | Portfolio</span>
           </p>
         </Link>
